a1eba68e68766191055bf56a8924531c75d16628
signl2025 <signl2025@proton.me>
2025-03-17 22:41:52 +0800

    Updated permissions
    For Android 13+ support.

--- lib/services/permission_service.dart
+++ lib/services/permission_service.dart
@@ -4,9 +4,17 @@ import 'package:permission_handler/permission_handler.dart';
 class PermissionService {
   static Future<void> requestPermissions() async {
     PermissionStatus storageStatus = await Permission.storage.status;
+    PermissionStatus audioStatus = await Permission.audio.status;
+    PermissionStatus videoStatus = await Permission.videos.status;
 
     if (!storageStatus.isGranted) {
       await Permission.storage.request();
+    } 
+    if (!audioStatus.isGranted) {
+      await Permission.audio.request();
+    } 
+    if (!videoStatus.isGranted) {
+      await Permission.videos.request();
     } 
   }
 }
